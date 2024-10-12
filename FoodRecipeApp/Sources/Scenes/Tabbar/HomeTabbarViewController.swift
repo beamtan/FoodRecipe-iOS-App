7c2394fd4b1485aa04e3e977b307c8ac5a67fe3c
beamtan <beamtantan@gmail.com>
2024-10-12 16:40:33 +0700

    Add animation for UITabbar

--- FoodRecipeApp/Sources/Scenes/Tabbar/HomeTabbarViewController.swift
+++ FoodRecipeApp/Sources/Scenes/Tabbar/HomeTabbarViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class HomeTabbarViewController: UITabBarController, UITabBarControllerDelegate {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -56,5 +55,63 @@ class HomeTabbarViewController: UITabBarController, UITabBarControllerDelegate {
     @objc func middleButtonPressed() {
         print("im here")
         UserDefaultService.shared.removeAll(completionHandler: nil)
+    }
+}
+
+// MARK: - Switching Tab Animation
+// Thanks to https://gist.github.com/illescasDaniel/6689461ccd396f0536e4f2e5f43befe8
+
+extension HomeTabbarViewController {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        if let fromView = tabBarController.selectedViewController?.view,
+           let toView = viewController.view, fromView != toView,
+           let controllerIndex = self.viewControllers?.firstIndex(of: viewController) {
+            
+            let viewSize = fromView.frame
+            let scrollRight = controllerIndex > tabBarController.selectedIndex
+            
+            // Avoid UI issues when switching tabs fast
+            if fromView.superview?.subviews.contains(toView) == true { return false }
+            
+            fromView.superview?.addSubview(toView)
+            
+            let screenWidth = UIScreen.main.bounds.size.width
+            toView.frame = CGRect(
+                x: (scrollRight ? screenWidth : -screenWidth),
+                y: viewSize.origin.y,
+                width: screenWidth,
+                height: viewSize.size.height
+            )
+            
+            UIView.animate(
+                withDuration: 0.25,
+                delay: TimeInterval(0.0),
+                options: [.curveEaseOut, .preferredFramesPerSecond60],
+                animations: {
+                    fromView.frame = CGRect(
+                        x: (scrollRight ? -screenWidth : screenWidth),
+                        y: viewSize.origin.y,
+                        width: screenWidth,
+                        height: viewSize.size.height
+                    )
+                    toView.frame = CGRect(
+                        x: 0,
+                        y: viewSize.origin.y,
+                        width: screenWidth,
+                        height: viewSize.size.height
+                    )
+                },
+                completion: { finished in
+                if finished {
+                    fromView.removeFromSuperview()
+                    tabBarController.selectedIndex = controllerIndex
+                }
+            })
+            
+            return true
+        }
+        
+        return false
     }
 }
